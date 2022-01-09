d6a1016ef03a7af7b9830311045065240d9b555b
Chiranjeev Singh <chiranjeevsingh140@gmail.com>
2022-01-10 03:39:46 +0530

    cc

--- AWS-CLI/s3move.sh
+++ AWS-CLI/s3move.sh
@@ -2,4 +2,5 @@
 
 #aws s3api create-bucket --bucket waddupppbucket --region us-east-1
 #aws s3 ls s3://cf-templates-1ejh6nnk3mgjq-us-east-1
-aws s3 cp AWS-CLI/ s3://waddupppbucket 
+#aws s3 cp AWS-CLI s3://waddupppbucket 
+aws s3 cp AWS-CLI s3://waddupppbucket --recursive
